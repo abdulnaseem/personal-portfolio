b637007dac0a856e9a7c4769793b43a28ad0e61a
Naseem <1610915@brunel.ac.uk>
2021-09-07 15:48:23 +0100

    updated urls

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,7 +18,8 @@ from django.urls import path, include
 from basic_portfolio import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
+    path('', views.uniprojects, name='uniprojects'),
     path('admin/', admin.site.urls),
     path('projects/', include('basic_portfolio.urls')),
 
