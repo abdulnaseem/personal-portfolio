a2c736c1d13357f2da861260259326fff6296c22
Naseem <1610915@brunel.ac.uk>
2021-11-29 12:07:23 +0000

    redesigned the index page

---
+++ static/js/index.js
@@ -0,0 +1,15 @@
+// const mobileTop = document.getElementById('mobile-top');
+// const mobileBottom = document.getElementById('mobile-bottom');
+//
+// if(window.innerWidth<600){
+//   mobileTop.innerHTML = "";
+//   mobileBottom.innerHTML = "";
+//   console.log("Emptied");
+//
+//   mobileTop.innerHTML = "<h1 class='mystack' align='left'>Familiarity of tools and technologies</h1><br>";
+//   mobileBottom.innerHTML = "<img id='mystack_collage' src='{% static 'images/famwith_collage.jpg' %}' alt=''>";
+//   console.log("Added");
+//   // mobileTop.insertAdjacentHTML('afterend', "<h1 class='mystack' align='left'>Familiarity of tools and technologies</h1><br>");
+//   // mobileBottom.insertAdjacentHTML('afterend', "<img id='mystack_collage' src='{% static 'images/famwith_collage.jpg' %}' alt=''>");
+//
+// }
---
+++ static/js/main.js
@@ -0,0 +1,39 @@
+// let nav = document.querySelector('nav');
+//
+// window.addEventListener('scroll', function () {
+//   if (window.pageYOffset > 100) {
+//     nav.classList.add('bg-dark', 'shadow');
+//   } else {
+//     nav.classList.remove('bg-dark', 'shadow');
+//   }
+// });
+// console.log('connected to main.js');
+
+// let myStack = document.getElementById('myStack');
+// let famWith = document.getElementById('famWith');
+//
+// let onScroll = function(){
+//   let y = window.scrollY;
+//   if (y > 180){
+//     myStack.className = 'container show';
+//     console.log('show');
+//   }
+//   else{
+//     myStack.className = 'container hide';
+//   }
+// }
+// window.addEventListener('scroll', onScroll);
+//
+//
+//
+// let onScroll2 = function(){
+//   let y = window.scrollY;
+//   if (y > 700){
+//     famWith.className = 'familiarwith_fullwidth show';
+//     console.log('show');
+//   }
+//   else{
+//     famWith.className = 'familiarwith_fullwidth hide';
+//   }
+// }
+// window.addEventListener('scroll', onScroll2);
